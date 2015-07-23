fb2075f3cd132d1030a253fc24718b129155c094
Ishan Khare <ishankhare07@gmail.com>
2015-07-23 14:38:12 +0530

    first commit with chapter 5 now working with different header files added a lib directory for getch and ungetch libraries

---
+++ chapter5/getint.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "../lib/get.h"
+#define SIZE 20
+
+int getint(int *pn) {
+    int c, sign;
+    while(isspace(c = getch()));        //skip whitespaces
+    if(!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetch(c);                     //not a number
+        return 0;
+    }
+
+    sign = (c == '-') ? -1 : 1;
+    if(c == '+' || c == '-') {
+        c = getch();
+    }
+
+    for(*pn = 0; isdigit(c); c = getch()) {
+        *pn = 10 * (*pn) + (c - '0');
+    }
+
+    *pn *= sign;
+    if(c != EOF) {
+        ungetch(c);
+    }
+
+    return c;
+}
+
+int main() {
+    int n, i, array[SIZE];
+    for(n = 0; n < SIZE && getint(&array[n]) != EOF; n++);
+
+    for(i = 0; i < n; i++) {
+        printf("%d\n",array[i]);
+    }
+    return 0;
+}
