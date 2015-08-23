285c213e30ce74731eb2cb35eda68787dcf368db
Ishan Khare <ishankhare07@gmail.com>
2015-08-23 22:09:40 +0530

    added code for question 5-4 program is a pointer version of strend

---
+++ chapter5/5-4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "../lib/my_getline.h"
+#define SIZE 50
+
+int strend(char*, char*);
+
+int main() {
+    char s[SIZE], t[SIZE];
+
+    getl(s, SIZE);
+    getl(t, SIZE);
+
+    printf("%d\n",strend(s, t));
+
+    return 0;
+}
+
+int strend(char *s, char *t) {
+    char *start = t;
+
+    while(*s) {
+        if(*s == *t) {
+            s++;
+            t++;
+        } else if(!(*t)) {
+            return 0;       // t exhausted but s not exhausted
+        } else {
+            s++;
+            t = start;
+        }
+    }
+
+    if(*t) {                // t not exhausted
+        return 0;           // not matched as s is exhausted
+    } else {
+        return 1;
+    }
+}
