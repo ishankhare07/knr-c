0e10b257cce3f38d841f0571b6aad3377d7a10b3
Ishan Khare <ishankhare07@gmail.com>
2015-09-06 19:30:29 +0530

    added code for binary tree

---
+++ chapter6/binarytree.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../lib/get.h"
+#define MAXWORD 100
+
+struct node {
+    char *word;
+    int count;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *addtree(struct node*, char *);
+int getword(char *, int);
+void treeprint(struct node*);
+
+int main() {
+    struct node *root;
+    char word[MAXWORD];
+
+    root = NULL;
+    while(getword(word, MAXWORD) != EOF) {
+        if(isalpha(word[0])) {
+            root = addtree(root, word);
+        }
+    }
+
+    treeprint(root);
+    return 0;
+}
+
+struct node *addtree(struct node *p, char *w) {
+    int cond;
+
+    if(p == NULL) {                                         // new word
+        p = (struct node*) malloc(sizeof (struct node));
+        p->word = strdup(w);
+        p->count = 1;
+        p->left = p->right = NULL;
+    } else if((cond = (strcmp(w, p->word)) == 0)) {           // repeated word
+        p->count++;
+    } else if(cond < 0) {                                   // into left subtree
+        p->left = addtree(p->left, w);
+    } else {
+        p->right = addtree(p->right, w);
+    }
+
+    return p;
+}
+
+int getword(char *word, int limit) {
+    int c;
+    char *w = word;
+
+    while(isspace(c = getch()));
+
+    if(c != EOF) {
+        *w++ = c;
+    }
+
+    if(!isalpha(c)) {
+        *w = '\0';
+        return c;
+    }
+
+    for( ; --limit > 0; w++) {
+        if(!isalnum(*w = getch())) {
+            ungetch(*w);
+            break;
+        }
+    }
+
+    *w = '\0';
+    return word[0];
+}
+
+void treeprint(struct node *p) {
+    if(p != NULL) {
+        treeprint(p->left);
+        printf("%4d %s\n", p->count, p->word);
+        treeprint(p->right);
+    }
+}
